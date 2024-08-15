3a62edabbb9f4fa4a64c21ed63ed55e95a381ac0
DaaAkuwuTumapel <saverodalta99@gmail.com>
2024-08-15 08:22:19 +0700

    fitur contact (belum update aowkkoo)

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = ({theme}) => {
   return (
     <AnimationOnScroll animateIn="animate__fadeInLeftBig">
     <div name='Contact' className={theme? 'w-full min-h-screen bg-[#ffffff] text-white flex justify-center items-center p-16' : 'w-full min-h-screen bg-[#0a192f] flex justify-center items-center p-16'}>
-        <form method='POST' action="https://getform.io/f/2a7c0aee-f42f-42d9-8c23-c7342fb81aeb" className='flex flex-col max-w-[600px] w-full'>
+        <form method='POST' action="https://forms.gle/QTFaHY4D8CgL71rs9" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8 max-w-[600px]'>
             <AnimationOnScroll initiallyVisible={true} duration={5} animateIn="animate__swing">
                 <p className={theme? 'text-4xl font-bold inline border-b-4 border-pink-600 text-black' : 'text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'}>Contact</p>
